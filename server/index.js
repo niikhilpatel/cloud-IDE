e6d77ff168a5770838f652702fdf1ed2d4827feb
Nikhil Patel <nikhilpatel26150@gmail.com>
2024-10-06 22:41:05 +0530

    pty added

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 const http = require('http')
 const express = require('express')
-const { Server: SocketServer} = require('socket.io')
+const { Server: SocketServer} = require('socket.io');
+const { Socket } = require('dgram');
+
+const app = express()
+const server = http.createServer(app);
+const io = new SocketServer({
+    cors: '*'
+}) 
+
+io.attach(server);
+
+ptyProcess.onData(data => {
+    io.emit('terminal:data', data)
+})
+
+io.on('connection', (Socket) => {
+    console.log(`Socket Connected`, socket.id)
+})
